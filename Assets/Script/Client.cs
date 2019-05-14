87f5a78f279dd9f426958f82ab423e2fdf351540
baealex <im@baejino.com>
2019-05-15 00:29:09 +0900

    [feat] socket client

--- Assets/Script/Client.cs
+++ Assets/Script/Client.cs
@@ -7,29 +7,66 @@ using System.Text;
 
 public class Client : MonoBehaviour
 {
+    string[] result;
     Socket sock;
     string cmd;
     byte[] receiverBuff;
+    ObjectPos[] mObjectPos;
+
+    class ObjectPos
+    {
+        private int posX;
+        private int posY;
+
+        private int desPosX;
+        private int desPosY;
+
+        public ObjectPos(int x, int y, int xd, int yd)
+        {
+            posX = x;
+            posY = y;
+            desPosX = xd;
+            desPosY = yd;
+        }
+
+        public int getPosX()
+        {
+            return posX;
+        }
+
+        public void printAllPos(int t)
+        {
+            Debug.Log("posX : " + posX);
+            Debug.Log("posY : " + posY);
+            Debug.Log("desPosX : " + desPosX);
+            Debug.Log("desPosY : " + desPosY);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        mObjectPos = new ObjectPos[4];
+
         sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         var ep = new IPEndPoint(IPAddress.Parse("175.122.48.163"), 8080);
         sock.Connect(ep);
 
         cmd = string.Empty;
         receiverBuff = new byte[8192];
-
-        Debug.Log("Connected...");
     }
 
     // Update is called once per frame
     void Update()
     {
         int n = sock.Receive(receiverBuff);
-
         string data = Encoding.UTF8.GetString(receiverBuff, 0, n);
+        result = data.Split(':');
 
-        Debug.Log(data);
+        for (int i = 0; i <= 12; i += 4)
+        {
+            mObjectPos[i / 4] = new ObjectPos(int.Parse(result[i]), int.Parse(result[i + 1]), int.Parse(result[i + 2]), int.Parse(result[i + 3]));
+            mObjectPos[i / 4].printAllPos(i / 4);
+        }
     }
 }
--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -1,11 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 public class Controller : MonoBehaviour
 {
 
     float speed;
+
+    string[] result;
+    Socket sock;
+    string cmd;
+    byte[] receiverBuff;
+    ObjectPos[] mObjectPos;
+
+    class ObjectPos
+    {
+        private int posX;
+        private int posY;
+
+        private int desPosX;
+        private int desPosY;
+
+        public ObjectPos(int x, int y, int xd, int yd)
+        {
+            posX = x;
+            posY = y;
+            desPosX = xd;
+            desPosY = yd;
+        }
+
+        public int getPosX()
+        {
+            return posX;
+        }
+        public int getPosY()
+        {
+            return posY;
+        }
+
+        public void printAllPos(int x)
+        {
+            Debug.Log(x + " posX : " + posX);
+            Debug.Log(x + " posY : " + posY);
+            Debug.Log(x + " desPosX : " + desPosX);
+            Debug.Log(x + " desPosY : " + desPosY);
+        }
+    }
 
     GameObject cube_red;
     GameObject cube_blue;
@@ -22,6 +65,15 @@ public class Controller : MonoBehaviour
 
     void Start()
     {
+        mObjectPos = new ObjectPos[4];
+
+        sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        var ep = new IPEndPoint(IPAddress.Parse("175.122.48.163"), 8080);
+        sock.Connect(ep);
+
+        cmd = string.Empty;
+        receiverBuff = new byte[8192];
+
         this.cube_red = GameObject.Find("Cube_Red");
         this.cube_blue = GameObject.Find("Cube_Blue");
         this.cube_green = GameObject.Find("Cube_Green");
@@ -46,22 +98,46 @@ public class Controller : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        int n = sock.Receive(receiverBuff);
+        string data = Encoding.UTF8.GetString(receiverBuff, 0, n);
+        result = data.Split(':');
+
+        for (int i = 0; i <= 12; i += 4)
+        {
+            mObjectPos[i / 4] = new ObjectPos(int.Parse(result[i]), int.Parse(result[i + 1]), int.Parse(result[i + 2]), int.Parse(result[i + 3]));
+            mObjectPos[i / 4].printAllPos(i / 4);
+        }
+
+        /*
+        this.new_cube_red.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime);
+        this.new_cube_blue.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime);
+        this.new_cube_green.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime);
+        this.new_cube_yellow.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime);
+        */
+ 
+        this.new_cube_red.transform.SetPositionAndRotation(Vector3.right * mObjectPos[0].getPosX() + Vector3.forward * mObjectPos[0].getPosY(), Quaternion.identity);
+        this.new_cube_green.transform.SetPositionAndRotation(Vector3.right * mObjectPos[1].getPosX() + Vector3.forward * mObjectPos[1].getPosY(), Quaternion.identity);
+        this.new_cube_blue.transform.SetPositionAndRotation(Vector3.right * mObjectPos[2].getPosX() + Vector3.forward * mObjectPos[2].getPosY(), Quaternion.identity);
+        this.new_cube_yellow.transform.SetPositionAndRotation(Vector3.right * mObjectPos[3].getPosX() + Vector3.forward * mObjectPos[3].getPosY(), Quaternion.identity);
+
+        // ------------------------------------------------------------------------------------------
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && false)
         {
             this.new_cube_red.transform.Translate(Vector3.right * speed /* * Time.smoothDeltaTime */, Space.World);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && false)
         {
             this.new_cube_red.transform.Translate(Vector3.right * speed /* * Time.smoothDeltaTime */ * -1, Space.World);
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && false)
         {
             this.new_cube_red.transform.Translate(Vector3.forward * speed /* * Time.smoothDeltaTime */, Space.World);
         }
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) && false)
         {
             this.new_cube_red.transform.Translate(Vector3.forward * speed /* * Time.smoothDeltaTime */ * -1, Space.World);
         }
@@ -71,6 +147,8 @@ public class Controller : MonoBehaviour
         this.new_cube_green.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime);
         this.new_cube_yellow.transform.Translate(Vector3.right * speed * Time.smoothDeltaTime);
         */
+
+        
     }
 }
 
